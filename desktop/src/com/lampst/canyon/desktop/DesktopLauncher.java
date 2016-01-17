b9e80a0e6002242cf928b9cc0445825a58af9c1f
khnguyendinh <khnguyendinh@gmail.com>
2016-01-17 23:24:24 +0700

    khoand
    upload Canyon bunny

---
+++ desktop/src/com/lampst/canyon/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.lampst.canyon.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.packtpub.libgdx.canyonbunny.CanyonBunnyMain;
+
+public class DesktopLauncher {
+	private static boolean rebuildAtlas = true;
+	private static boolean drawDebugOutline = true;
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CanyonBunnyMain(), config);
+	}
+}
